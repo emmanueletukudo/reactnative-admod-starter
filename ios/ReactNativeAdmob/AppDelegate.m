cda6f3b2f97897e830cf9aa8e6678709b5ec82da
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-05-11 14:50:06 +0100

    update on xcode project

--- ios/ReactNativeAdmob/AppDelegate.m
+++ ios/ReactNativeAdmob/AppDelegate.m
@@ -27,7 +27,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  if ([FIRApp defaultApp] == nil) {
+ if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
   }
 #ifdef FB_SONARKIT_ENABLED
