401ebcc10d3210fed0c98c1ac4f725c86e74fa5d
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-05-11 11:10:51 +0100

    added showRewardAd and showInterstitialAd methods

--- App.js
+++ App.js
@@ -15,19 +15,65 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
 import {TestIds, BannerAd, BannerAdSize} from '@react-native-firebase/admob';
 
 const App = () => {
+  showInterstitialAd = () => {
+    interstitialAd.onAdEvent((type, error) => {
+      if (type === AdEventType.LOADED) {
+        interstitialAd.show();
+      }
+    });
+    interstitialAd.load();
+  };
+
+  showRewardAd = () => {
+    // Create a new instance
+    const rewardAd = RewardedAd.createForAdRequest(TestIds.REWARDED);
+    // Add event handlers
+    rewardAd.onAdEvent((type, error) => {
+      if (type === RewardedAdEventType.LOADED) {
+        rewardAd.show();
+      }
+
+      if (type === RewardedAdEventType.EARNED_REWARD) {
+        console.log('User earned reward of 3 stars');
+        Alert.alert(
+          'New Reward',
+          'You just earned a reward of 3 stars',
+          [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+          {cancelable: true},
+        );
+      }
+    });
+
+    // Load a new advert
+    rewardAd.load();
+  };
+
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
-          <Text>Reward Ads</Text>
+          <Text>Interstitial Ads</Text>
+          <Button
+            title="Show Reward Ad"
+            color="white"
+            onPress={() => showInterstitialAd()}
+            style={styles.btn}
+          />
         </View>
         <View>
-          <Text>Interstitial Ads</Text>
+          <Text>Reward Ads </Text>
+          <Button
+            title="Show Reward Ad"
+            color="white"
+            onPress={() => showRewardAd()}
+            style={styles.btn}
+          />
         </View>
         <View>
           <BannerAd
